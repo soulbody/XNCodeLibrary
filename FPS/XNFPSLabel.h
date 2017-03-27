97374ccf11cd4c8a9f6bd10fe27bc025f4e19134
xuning <xuninghsu@gmail.com>
2017-03-27 13:32:28 +0800

    FPS

---
+++ FPS/XNFPSLabel.h
@@ -0,0 +1,13 @@
+//
+//  XNFPSLabel.h
+//  XingNLive
+//
+//  Created by xuning on 2017/3/13.
+//  Copyright © 2017年 XingNLive&Mall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XNFPSLabel : UILabel
+
+@end
